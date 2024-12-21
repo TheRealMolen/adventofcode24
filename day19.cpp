c5a40fa8d20ebb73f084cf109be33f02bf33885e
Alex Mole <alex.molen@gmail.com>
2024-12-21 15:32:00 +0000

    day19

--- day19.cpp
+++ day19.cpp
@@ -2,37 +2,142 @@
 #include "harness.h"
 
 
-int day19(const stringlist& input)
+bool pattern_is_possible(const string& target, const vector<vector<string>>& availableByFirstChar, vector<u8>& ruledOutBeyond, size_t offs=0)
 {
-    for (auto& line : input)
+    if (offs == target.length())
+        return true;
+    if (offs > target.length())
+        return false;
+    if (ruledOutBeyond[offs])
+        return false;
+
+    const char* pCurrTargetBase = target.c_str() + offs;
+
+    for (const string& pattern : availableByFirstChar[*pCurrTargetBase - 'a'])
     {
-        (void)line;
+        const char* pCurrTarget = pCurrTargetBase;
+        const char* pCurrPatt = pattern.c_str();
+
+        // we know the first chars match
+        ++pCurrTarget;
+        ++pCurrPatt;
+
+        // skip to end of shortest, or first mismatch
+        for (; *pCurrTarget == *pCurrPatt && *pCurrTarget; ++pCurrTarget, ++pCurrPatt)
+        { /**/ }
+
+        // mismatch detected before end of pattern; that's a no
+        if (*pCurrPatt)
+            continue;
+
+        size_t newOffs = offs + pattern.length();
+        if (pattern_is_possible(target, availableByFirstChar, ruledOutBeyond, newOffs))
+            return true;
+        else
+            ruledOutBeyond[newOffs] = 1;
     }
 
-    return -1;
+    return false;
 }
 
-int day19_2(const stringlist& input)
+int day19(const stringlist& input)
 {
-    for (auto& line : input)
+    vector<vector<string>> availableByFirstChar('z' - 'a', {});
+    for (string& avail : split(input.front(), ", "s))
+        availableByFirstChar[avail[0] - 'a'].emplace_back(move(avail));
+    for (vector<string>& patterns : availableByFirstChar)
+        ranges::sort(patterns);
+
+    int numPossible = 0;
+    for (auto& target : input | views::drop(2))
     {
-        (void)line;
+        vector<u8> ruledOutBeyond(size(target), 0);
+        const bool possible = pattern_is_possible(target, availableByFirstChar, ruledOutBeyond);
+        if (possible)
+            ++numPossible;
     }
 
-    return -1;
+    return numPossible;
+}
+
+
+
+void count_solutions(const string& target, const vector<vector<string>>& availableByFirstChar, vector<i64>& solutionsForRemainder, size_t offs = 0)
+{
+    const size_t targetLength = target.length();
+    assert(offs < targetLength);
+
+    const char* pCurrTargetBase = target.c_str() + offs;
+
+    for (const string& pattern : availableByFirstChar[*pCurrTargetBase - 'a'])
+    {
+        const size_t patternLength = pattern.length();
+        if (offs + patternLength > targetLength)
+            continue;
+
+        const char* pCurrTarget = pCurrTargetBase;
+        const char* pCurrPatt = pattern.c_str();
+
+        // we know the first chars match
+        ++pCurrTarget;
+        ++pCurrPatt;
+
+        // skip to end of shortest, or first mismatch
+        for (; *pCurrTarget == *pCurrPatt && *pCurrTarget; ++pCurrTarget, ++pCurrPatt)
+        { /**/ }
+
+        // mismatch detected before end of pattern; that's a no
+        if (*pCurrPatt)
+            continue;
+
+        size_t newOffs = offs + patternLength;
+        if (newOffs == targetLength)
+            ++solutionsForRemainder[offs];
+        else
+            solutionsForRemainder[offs] += solutionsForRemainder[newOffs];
+    }
+}
+
+i64 day19_2(const stringlist& input)
+{
+    vector<vector<string>> availableByFirstChar('z' - 'a', {});
+    for (string& avail : split(input.front(), ", "s))
+        availableByFirstChar[avail[0] - 'a'].emplace_back(move(avail));
+    for (vector<string>& patterns : availableByFirstChar)
+        ranges::sort(patterns);
+
+    i64 numSolutions = 0;
+    for (const auto& target : input | views::drop(2))
+    {
+        vector<i64> solutionsForRemainder(size(target), 0);
+
+        for (size_t offs = target.length() - 1; offs < target.length(); --offs)
+            count_solutions(target, availableByFirstChar, solutionsForRemainder, offs);
+
+        numSolutions += solutionsForRemainder.front();
+    }
+
+    return numSolutions;
 }
 
 
 void run_day19()
 {
     string sample =
-R"(beep
-boop
-blarp)";
+R"(r, wr, b, g, bwu, rb, gb, br
 
-    test(-100, day19(READ(sample)));
-    //gogogo(day19(LOAD(19)));
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb)";
 
-    //test(-100, day19_2(READ(sample)));
-    //gogogo(day19_2(LOAD(19)));
+    test(6, day19(READ(sample)));
+    gogogo(day19(LOAD(19)));
+
+    test(16, day19_2(READ(sample)));
+    gogogo(day19_2(LOAD(19)));
 }
--- main.cpp
+++ main.cpp
@@ -59,12 +59,12 @@ int main()
         run_day15();
         run_day16();
         run_day17();
+        run_day18();
     }
     {
         TIME_SCOPE(new);
 
-        run_day18();
-        //run_day19();
+        run_day19();
         //run_day20();
         //run_day21();
         //run_day22();
